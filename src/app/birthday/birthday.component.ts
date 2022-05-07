01cc206dc8f6bce756b40cc444d39625497e7d83
Juan Herrera <david.juanherrera@gmail.com>
2022-05-07 15:35:55 +0200

    add new url read

--- src/app/birthday/birthday.component.ts
+++ src/app/birthday/birthday.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { pluck } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({ templateUrl: './birthday.component.html' })
 export class BirthdayComponent implements OnInit {
-  name$: Observable<string>;
+  name: string;
+  // guests = [
+  //   'maria',
+  //   'carsten',
+  //   'thomas',
+  //   'andrea',
+  //   'verena',
+  //   'clara',
+  //   'kilian',
+  //   'larissa',
+  //   'paolo',
+  //   'jan',
+  //   'jose',
+  //   'konny',
+  //   'anna',
+  //   'silvia',
+  //   'julien',
+  //   'jonathan',
+  //   'sam',
+  //   'katy',
+  //   'rubaeat',
+  // ];
 
   constructor(private activatedRoute: ActivatedRoute) {
-    this.name$ = this.activatedRoute.paramMap.pipe(pluck('params', 'name')) as Observable<string>;
+    this.name = this.activatedRoute.snapshot.paramMap.get('name') || 'friend';
   }
 
   ngOnInit(): void {}
