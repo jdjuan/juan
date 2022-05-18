08fdecf6bc318c6d62e4111f808dd160beb3a25b
Juan Herrera <david.juanherrera@gmail.com>
2022-05-18 16:33:24 +0200

    feat: add new date

--- src/app/birthday/birthday.component.ts
+++ src/app/birthday/birthday.component.ts
@@ -4,27 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 @Component({ templateUrl: './birthday.component.html' })
 export class BirthdayComponent implements OnInit {
   name: string;
-  // guests = [
-  //   'maria',
-  //   'carsten',
-  //   'thomas',
-  //   'andrea',
-  //   'verena',
-  //   'clara',
-  //   'kilian',
-  //   'larissa',
-  //   'paolo',
-  //   'jan',
-  //   'jose',
-  //   'konny',
-  //   'anna',
-  //   'silvia',
-  //   'julien',
-  //   'jonathan',
-  //   'sam',
-  //   'katy',
-  //   'rubaeat',
-  // ];
 
   constructor(private activatedRoute: ActivatedRoute) {
     this.name = this.activatedRoute.snapshot.paramMap.get('name') || 'friend';
